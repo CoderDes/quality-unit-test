c025e11811bece82db46043dc78717fcd5629fff
Eugene <eugzubv@gmail.com>
2020-09-29 10:07:40 +0300

    filter C-lines when serviceId and questionId are set

--- src/com/eugz/Main.java
+++ src/com/eugz/Main.java
@@ -20,17 +20,127 @@ public class Main {
 
 //            For debug purposes ===
             lines.forEach(lineItem -> {
-                if (lineItem instanceof CLineItem) {
-                    System.out.println("This is C line!");
-                } else if (lineItem instanceof DLineItem) {
+                if (lineItem instanceof DLineItem) {
                     System.out.println("This is D line!");
+                    List<LineItem> subListCLines = new ArrayList<>();
                 }
             });
 //            For debug purposes ===
+
+            for (int i = 0; i < lines.size(); i++) {
+                LineItem current = lines.get(i);
+
+                if (current instanceof DLineItem) {
+                    List<CLineItem> cLineItemsBeforeCurrent = getCLineItemBeforeIndex(lines, i);
+
+                    if (current.isServiceMatchAll() && current.isQuestionTypeIdMatchAll()) {
+//                    Match all case
+
+                    } else if (current.isServiceMatchAll() && !current.isQuestionTypeIdMatchAll()) {
+//                    Certain serviceId case and certain questionTypeId case
+
+                    } else if (!current.isServiceMatchAll() && current.isQuestionTypeIdMatchAll()) {
+//                    Match all serviceId case and certain questionTypeId case
+
+                    } else {
+//                    Certain serviceId case and match all questionTypeId case
+                        int serviceId = current.getServiceId();
+                        Integer variationId = current.getVariationId();
+                        int questionTypeId = current.getQuestionTypeId();
+                        Integer categoryId = current.getCategoryId();
+                        Integer subcategoryId = current.getSubCategoryId();
+
+
+                    }
+
+                }
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+//    TODO: refactor to separate entity ===========================================================
+
+    public static List<CLineItem> getCLineItemBeforeIndex(List<LineItem> list, int dItemIndex) {
+        List<CLineItem> items = new ArrayList<>();
+
+        for (int i = 0; i < dItemIndex; i++) {
+            LineItem item = items.get(i);
+            if (item instanceof CLineItem) {
+                items.add((CLineItem) item);
+            }
+        }
+
+        return items;
+    }
+
+    public static List<CLineItem> getResultCLines(List<CLineItem> list, int serviceId, Integer variationId, int questionTypeId, Integer categoryId, Integer subcategoryId)  {
+        List<CLineItem> filteredByService = filterByService(list, serviceId, variationId);
+        List<CLineItem> filteredByAll = filterByQuestionType(filteredByService, questionTypeId, categoryId, subcategoryId);
+
+        return filteredByAll;
+    }
+
+    public static List<CLineItem> filterByService(List<CLineItem> list, int serviceId, Integer variationId) {
+        List<CLineItem> byServiceOnly = new ArrayList<>();
+
+        for (CLineItem item : list) {
+            if (item.getServiceId() == serviceId) {
+                byServiceOnly.add(item);
+            }
+        }
+
+        if (variationId != null) {
+            List<CLineItem> byServiceAndVariation = new ArrayList<>();
+
+            for (CLineItem item : byServiceOnly) {
+                if (item.getVariationId() == variationId) {
+                    byServiceAndVariation.add(item);
+                }
+            }
+
+            return byServiceAndVariation;
+        }
+
+        return byServiceOnly;
+    }
+
+    public static List<CLineItem> filterByQuestionType(List<CLineItem> list, int questionTypeId, Integer categoryId, Integer subcategoryId) {
+        List<CLineItem> byQuestionTypeIdOnly = new ArrayList<>();
+
+        for (CLineItem item : list) {
+            if (item.getQuestionTypeId() == questionTypeId) {
+                byQuestionTypeIdOnly.add(item);
+            }
+        }
+
+        if (categoryId != null) {
+            List<CLineItem> byQuestionTypeAndCategory = new ArrayList<>();
+
+            for (CLineItem item : byQuestionTypeIdOnly) {
+                if (item.getCategoryId() == categoryId) {
+                    byQuestionTypeAndCategory.add(item);
+                }
+            }
+
+            if (subcategoryId != null) {
+                List<CLineItem> byQuestionAndCategoryAndSubcategory = new ArrayList<>();
+
+                for (CLineItem item : byQuestionTypeAndCategory) {
+                    if (item.getSubCategoryId() == subcategoryId) {
+                        byQuestionAndCategoryAndSubcategory.add(item);
+                    }
+                }
+
+                return byQuestionAndCategoryAndSubcategory;
+            }
+
+            return byQuestionTypeAndCategory;
+        }
+
+        return byQuestionTypeIdOnly;
     }
 }
